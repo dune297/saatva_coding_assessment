8462e54959683ece6ee1a97a5a9096e83ce94f3c
dune297 <dune@utexas.edu>
2020-08-20 14:44:47 -0500

    Ran prettier

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -2,32 +2,37 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class ButtonGroup extends Component {
-	static propTypes = {
-		buttonConfig: PropTypes.object,
-		handleSelect: PropTypes.func
-	}
+  static propTypes = {
+    buttonConfig: PropTypes.object,
+    handleSelect: PropTypes.func,
+  };
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	render() {
-		const { buttonConfig, handleSelect } = this.props;
+  render() {
+    const { buttonConfig, handleSelect } = this.props;
 
-
-
-		return (
-			<div>
-				<p className="buttonGroupHeader">{buttonConfig.header}</p>
-				<div className="buttonWrapper">
-					{ buttonConfig.labels.map(label => 
-						(<button key={label} className={label === buttonConfig.selected ? "selectedButton" : "unselectedButton"}
-							onClick={() => handleSelect(label)}>
-								{label}
-							</button>)
-					) }
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div>
+        <p className="buttonGroupHeader">{buttonConfig.header}</p>
+        <div className="buttonWrapper">
+          {buttonConfig.labels.map((label) => (
+            <button
+              key={label}
+              className={
+                label === buttonConfig.selected
+                  ? "selectedButton"
+                  : "unselectedButton"
+              }
+              onClick={() => handleSelect(label)}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/InfoWidget.js
+++ src/components/InfoWidget.js
@@ -1,65 +1,81 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ButtonGroup from "./ButtonGroup";
-import StarIcon from "../resources/star-icon.svg"
+import StarIcon from "../resources/star-icon.svg";
 
 export default class InfoWidget extends Component {
-	static propTypes = {
-		mattressData: PropTypes.Object,
-		incrementCartCount: PropTypes.func,
-		handleSelect: PropTypes.func,
-		selected: PropTypes.string
-	}
+  static propTypes = {
+    mattressData: PropTypes.Object,
+    incrementCartCount: PropTypes.func,
+    handleSelect: PropTypes.func,
+    selected: PropTypes.string,
+  };
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	addToCartButton = () => {
-		return <button className="addToCartButton" onClick={this.props.incrementCartCount}>Add To Cart</button>
-	};
+  addToCartButton = () => {
+    return (
+      <button
+        className="addToCartButton"
+        onClick={this.props.incrementCartCount}
+      >
+        Add To Cart
+      </button>
+    );
+  };
 
-	additionalInfo = (selected, mattressData) => {
-		var currencyFormat = { 
-			style: 'currency', 
-			currency: 'USD', 
-			minimumFractionDigits: 0,
-			maximumFractionDigits: 0 
-		};
+  additionalInfo = (selected, mattressData) => {
+    var currencyFormat = {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    };
 
-		const selectedMattress = mattressData[selected];
+    const selectedMattress = mattressData[selected];
 
-		return (
-			<div className="additionalInfo"> 
-				<p className="selectedName">
-						{`${selectedMattress.name} `}
-						<span className="selectedRating">
-							({`${selectedMattress.reviewRating}`}
-							<StarIcon viewBox="-4 5 32 16" width="18" height="16" />)
-						</span>
-				</p>
-				<p className="selectedPrice"> {new Intl.NumberFormat('en-US', currencyFormat)
-					.format(selectedMattress.price)} </p>
-			</div>
-	)};
+    return (
+      <div className="additionalInfo">
+        <p className="selectedName">
+          {`${selectedMattress.name} `}
+          <span className="selectedRating">
+            ({`${selectedMattress.reviewRating}`}
+            <StarIcon viewBox="-4 5 32 16" width="18" height="16" />)
+          </span>
+        </p>
+        <p className="selectedPrice">
+          {" "}
+          {new Intl.NumberFormat("en-US", currencyFormat).format(
+            selectedMattress.price
+          )}{" "}
+        </p>
+      </div>
+    );
+  };
 
-	render() {
-		const { selected, handleSelect, mattressData } = this.props;
+  render() {
+    const { selected, handleSelect, mattressData } = this.props;
 
-		const buttonConfig = {
-			labels: Object.keys(mattressData).map(mattress => mattressData[mattress].name),
-			header: "SELECT MATTRESS TYPE",
-			selected: mattressData[selected].name
-		};
+    const buttonConfig = {
+      labels: Object.keys(mattressData).map(
+        (mattress) => mattressData[mattress].name
+      ),
+      header: "SELECT MATTRESS TYPE",
+      selected: mattressData[selected].name,
+    };
 
-		return (
-			<div>
-				<p className='chooseHeader'>Choose Your Mattress</p>
-				<ButtonGroup buttonConfig={buttonConfig} 
-					handleSelect={handleSelect}></ButtonGroup>
-				{ this.additionalInfo(selected, mattressData) }
-				{ this.addToCartButton() }
-			</div>	
-		)	
-	}
+    return (
+      <div>
+        <p className="chooseHeader">Choose Your Mattress</p>
+        <ButtonGroup
+          buttonConfig={buttonConfig}
+          handleSelect={handleSelect}
+        ></ButtonGroup>
+        {this.additionalInfo(selected, mattressData)}
+        {this.addToCartButton()}
+      </div>
+    );
+  }
 }
